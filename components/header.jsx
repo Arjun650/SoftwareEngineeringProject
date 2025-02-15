c9ded6b8422a07762af8441b98ad626c871f4590
Arjun650 <darjun650@gmail.com>
2025-02-15 19:06:54 +0530

    first commit for setup

---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './toggle'
+
+const Header = () => {
+    return (
+        <div>header
+            <ModeToggle />
+        </div>
+    )
+}
+
+export default Header
