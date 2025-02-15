d69ddbdc32decbe7e6fbc80c93e678fa9a5d28d9
Arjun650 <darjun650@gmail.com>
2025-02-15 23:12:01 +0530

    added signin with clerk

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { ModeToggle } from './toggle'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const Header = () => {
     return (
-        <div>header
+        <header>
+            <nav>
+                <Link href='/'>LOGO</Link>
+            </nav>
+
+
+        <div>
             <ModeToggle />
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
+        </header>
     )
 }
 
