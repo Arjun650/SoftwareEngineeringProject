555ba40b97f0da888fd2ce8034cac44472d19fce
Arjun650 <darjun650@gmail.com>
2025-02-16 10:33:01 +0530

    navbar

--- components/header.jsx
+++ components/header.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { ModeToggle } from './toggle'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
+import { Button } from './ui/button'
+import { LayoutDashboard } from 'lucide-react'
 
 const Header = () => {
     return (
-        <header>
-            <nav>
-                <Link href='/'>LOGO</Link>
+        <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+            <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+                <Link className='h-12 py-1 w-auto object-contain' href='/'>LOGO</Link>
+                <div>
+                    <SignedIn>
+                        <Link href={"/dashboard"}>
+                            <Button>
+                                <LayoutDashboard className='h-4 w-4'/>
+                                Industry Insights
+                            </Button>
+                        </Link>
+                    </SignedIn>
+                </div>
             </nav>
 
 
