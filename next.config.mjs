303beb0443aa990710ef01366e7d51713e88ae98
Arjun650 <darjun650@gmail.com>
2025-04-05 00:14:26 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "randomuser.me" }],
   },
+  eslint:{
+    ignoreDuringBuilds:true,
+  }
 };
 
 export default nextConfig;
